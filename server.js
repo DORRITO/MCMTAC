9534e7fa9c040c7e8562a7a02f4860d4a0ab4e96
CHIp Wright <frisbeeprod@msn.com>
2018-01-29 03:21:09 -0700

    join

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const path = require('path');
-// const publicPath = path.join(__dirname, './client'); //maybe this?
+const publicPath = path.join(__dirname, './client/build'); //maybe this?
 const express = require('express');
 const app = express();
 var mongoose = require('mongoose');
@@ -11,8 +11,8 @@ const _ = require('lodash');
 // app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 const port = process.env.PORT || 8000;
-// app.use(express.static(publicPath));
-app.use('/', express.static(`${__dirname}/client/public`)); //or this?
+app.use(express.static(publicPath));
+// app.use('/', express.static(`${__dirname}/client/build`)); //or this?
 
 let {PlayersAPI} = require('./server/models/players');
 
