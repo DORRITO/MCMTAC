0c40cedd90481667633b476c809207049376d914
CHIp Wright <frisbeeprod@msn.com>
2018-01-29 03:46:38 -0700

    turned off path

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const publicPath = path.join(__dirname, './client/build'); //maybe this?
+// const publicPath = path.join(__dirname, './client/build'); //maybe this?
 const express = require('express');
 const app = express();
 var mongoose = require('mongoose');
@@ -11,7 +11,7 @@ const _ = require('lodash');
 // app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 const port = process.env.PORT || 8000;
-app.use(express.static(publicPath));
+// app.use(express.static(publicPath));
 // app.use('/', express.static(`${__dirname}/client/build`)); //or this?
 
 let {PlayersAPI} = require('./server/models/players');
