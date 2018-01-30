0475276b4456e3b94715bca6cff7a6a55f3b37f1
CHIp Wright <frisbeeprod@msn.com>
2018-01-29 18:30:30 -0700

    trying without path

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const path = require('path');
-// const publicPath = path.join(__dirname, './client/build'); //maybe this?
 const express = require('express');
 const app = express();
 var mongoose = require('mongoose');
@@ -11,8 +10,7 @@ const _ = require('lodash');
 // app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 const port = process.env.PORT || 8000;
-// app.use(express.static(publicPath));
-app.use('/', express.static(`${__dirname}/client/build`)); //or this?
+// app.use(express.static(`${__dirname}/client/build`)); //or this?
 
 let {PlayersAPI} = require('./server/models/players');
 
