70938b6a92597a8c32e68a0d6571c99e29ec7e30
CHIp Wright <frisbeeprod@msn.com>
2018-03-26 23:59:45 -0700

    health

--- server/models/players.js
+++ server/models/players.js
@@ -52,7 +52,7 @@ let newPlayers = new PlayersAPI({
             race: 'Elf',
             class: 'Hunter',
             profession: 'Scout, spy',
-            stats: {},
+            stats: {health: 'full'},
             affiliation: 'None',
             dice: '',
             login: 'wolfbane',
